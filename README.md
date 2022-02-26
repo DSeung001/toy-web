d300b3b87a710a4bb0949ab49154f3977e4e00a7
seungryeol <seungryeol156@gmail.com>
2022-02-26 23:18:28 +0900

    readme 추가

---
+++ README.md
@@ -0,0 +1,3 @@
+[나무그리기](https://seungr.github.io/web-art/tree/)
+- 아무 곳이나 클릭하면 나무가 생겨요
+<hr/>
--- tree/src/js/tree.js
+++ tree/src/js/tree.js
@@ -77,6 +77,5 @@ export class Tree {
 
     random(min, max){
         return min + Math.floor(Math.random() * (max - min + 1));
-
     }
 }
